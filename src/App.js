8c1b7ee963e2fe2f432f20fe810fb32ef1aa3c41
Winnie Wu <weiwinnie.wu@gmail.com>
2018-04-15 17:05:11 -0700

    Add basic random aeropress recipe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import aeropress from './aeropress.svg';
+import  Recipe from './Recipe.js';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={aeropress} className="App-logo" alt="logo" />
+          <h1 className="App-title">Random Aeropress Recipe</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Recipe/>
+        <footer className="App-footer">
+          <p>
+            Built with &hearts; by <a href="https://github.com/thewinniewu">thewinniewu</a>
+          <br/>
+          Aeropress icon made by <a href="https://www.flaticon.com/authors/monkik">monkik</a>
+          </p>
+        </footer>
       </div>
     );
   }
---
+++ src/Recipe.js
@@ -0,0 +1,181 @@
+import React from 'react';
+
+const COFFEE_WATER_RATIOS = [
+  { coffee: 23, water: 200 },
+  { coffee: 18, water: 250 },
+  { coffee: 15, water: 250 },
+  { coffee: 12, water: 200 }
+];
+
+const GRIND_BREWTIME_RATIOS = [
+  { grind: 'fine', time: 60 },
+  { grind: 'medium', time: 90 },
+  { grind: 'coarse', time: 120 }
+];
+
+const BLOOM_SECONDS = [20, 30, 40];
+const BLOOM_WATER_G = [30, 60];
+const BREW_TEMP_C = [80, 85, 90, 95];
+const CLOCKWISE_STIR_TIMES = [0, 1, 2];
+
+function randomElement(items) {
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function formatTimeEnglish(times) {
+  if (times === 1) {
+    return 'once';
+  } else {
+    return '' + times + ' times';
+  }
+}
+
+class Recipe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      started: false,
+      inverted: false,
+      bloomSeconds: BLOOM_SECONDS[0],
+      bloomWaterG: BLOOM_WATER_G[0],
+      brewTempC: BREW_TEMP_C[0],
+      coffeeWaterRatio: COFFEE_WATER_RATIOS[0],
+      grindBrewTime: GRIND_BREWTIME_RATIOS[0],
+      clockwiseStirTimes: CLOCKWISE_STIR_TIMES[0],
+      anticlockwiseStir: true,
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      started: true,
+      inverted: randomElement([true, false]),
+      bloomSeconds: randomElement(BLOOM_SECONDS),
+      bloomWaterG: randomElement(BLOOM_WATER_G),
+      brewTempC: randomElement(BREW_TEMP_C),
+      coffeeWaterRatio: randomElement(COFFEE_WATER_RATIOS),
+      grindBrewTime: randomElement(GRIND_BREWTIME_RATIOS),
+      clockwiseStirTimes: randomElement(CLOCKWISE_STIR_TIMES),
+      anticlockwiseStir: randomElement([true, false]),
+    });
+  }
+
+  renderHeatWaterStep() {
+    return <li>
+      Heat {this.state.coffeeWaterRatio.water}g of water
+      to {this.state.brewTempC}°C / {toFahrenheit(this.state.brewTempC)}°F.
+    </li>;
+  }
+
+  renderGrindCoffeeStep() {
+    return <li>
+      Grind {this.state.coffeeWaterRatio.coffee}g of coffee to a {this.state.grindBrewTime.grind} grind.
+    </li>;
+  }
+
+  renderPourCoffeeStep() {
+    return <li>
+      Pour in the ground coffee.
+    </li>;
+  }
+
+  renderInvertStep() {
+    if (this.state.inverted) {
+      return <li>Place the aeropress in the upside-down orientation.</li>
+    } else {
+      return <li>Place the aeropress on the mug in the normal orientation with wet filter and cap on.</li>
+    }
+  }
+
+  renderBloomStep() {
+    if (this.state.bloomWaterG > 0) {
+      return <li>
+        Add {this.state.bloomWaterG}g of water and wait {this.state.bloomSeconds} seconds
+        for the coffee to bloom.
+      </li>;
+    }
+  }
+
+  renderAddWaterStep() {
+    if (this.state.bloomWaterG > 0) {
+      return <li>
+        Add the remaining {this.state.coffeeWaterRatio.water - this.state.bloomWaterG}g of water.
+      </li>;
+    } else {
+      return <li>
+        Add all the water ({this.state.coffeeWaterRatio.water}g).
+      </li>;
+    }
+  }
+
+  renderBrewStep() {
+    return <li>
+      Wait {this.state.grindBrewTime.time}s to brew.
+    </li>;
+  }
+
+  renderStirStep() {
+    if (this.state.clockwiseStirTimes > 0) {
+      let instruction = ''
+      instruction += 'Stir ' + formatTimeEnglish(this.state.clockwiseStirTimes) + ' in one direction.';
+      if (this.state.anticlockwiseStir) {
+        instruction += ' Repeat in the other direction.'
+      }
+      return <li>
+        { instruction }
+      </li>
+    }
+  }
+
+  renderEndInvertStep() {
+    if (this.state.inverted) {
+      return <li>
+        Wet the filter paper, and put the cap on.
+        Place the mug upside-down on the aeropress and flip to be the normal orientation.
+      </li>
+    }
+  }
+
+  renderRecipe() {
+    if (this.state.started) {
+      return <ol>
+        { this.renderHeatWaterStep() }
+        { this.renderGrindCoffeeStep() }
+        { this.renderInvertStep() }
+        { this.renderPourCoffeeStep() }
+        { this.renderBloomStep() }
+        { this.renderAddWaterStep() }
+        { this.renderStirStep() }
+        { this.renderBrewStep() }
+        { this.renderEndInvertStep() }
+        <li>
+          Press.
+        </li>
+      </ol>;
+    }
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-12">
+          <h1>
+            <button className="btn btn-primary" onClick={this.handleClick.bind(this)}>Generate a Recipe</button>
+          </h1>
+          <div className="card">
+            <div className="card-block">
+              { this.renderRecipe() }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Recipe;
